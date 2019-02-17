9df2cfa63c16eab9d9b5e848cd14f854e98b0f06
saigal15 <46360205+saigal15@users.noreply.github.com>
2019-02-17 23:48:11 +0100

    typrscriptArithmetic

---
+++ addition.js
@@ -0,0 +1,13 @@
+function addition(num1, num2) {
+    return (num1 + num2);
+}
+console.log("add(5,5) = " + addition(5, 5));
+function substraction(a, b) {
+    return (a - b);
+}
+console.log("sub(10,5) = " + substraction(10, 5));
+function multiply(a, b) {
+    return (a * b);
+}
+console.log("mul(5,5) = " + multiply(5, 5));
+//# sourceMappingURL=addition.js.map
---
+++ addition.ts
@@ -0,0 +1,13 @@
+function addition(num1:number,num2:number)
+{
+    return(num1 + num2)
+}
+console.log("add(5,5) = " + addition(5,5))
+function substraction(a:number,b:number){
+    return(a - b)
+}
+console.log("sub(10,5) = " + substraction(10,5))
+function multiply(a:number,b:number){
+    return(a * b)
+}
+console.log("mul(5,5) = " + multiply(5,5))
